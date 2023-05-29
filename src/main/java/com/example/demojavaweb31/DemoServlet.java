47076ee17bd3eb2ce1457edde4956a71fc1d3bd2
Nguyen Thanh Luan <luann4099@gmail.com>
2023-05-29 20:51:28 +0700

    update code

---
+++ src/main/java/com/example/demojavaweb31/DemoServlet.java
@@ -0,0 +1,16 @@
+package com.example.demojavaweb31;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello World!</h1>");
+    }
+}
